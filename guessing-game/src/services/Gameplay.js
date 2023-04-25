08b19cb6045892724597697d186d23dc3e961341
Marla Vasquez <vasquez.marlasabado@gmail.com>
2023-04-25 18:38:54 +0800

    Add comments

--- guessing-game/src/services/Gameplay.js
+++ guessing-game/src/services/Gameplay.js
@@ -4,15 +4,15 @@ import { CATEGORIES } from './Constants';
 
 export const newGame = async () => {
     console.log("preparing round");
-    const randomIndex =  Math.floor(Math.random() * 10);
-    const category = CATEGORIES[randomIndex];
-    const  word =  await generateWordByCategory(category);
-    const  clues =  await generateClues(word, category);
+    const randomIndex =  Math.floor(Math.random() * 10); //randomize index here
+    const category = CATEGORIES[randomIndex]; // get category based on index
+    const  word =  await generateWordByCategory(category); // get clue from chatgpt
+    const  clues =  await generateClues(word, category); // get hints from chatgrt
     const roundDetails = {
         "categoy": category,
         "word": word,
         "clues": clues
-    };
+    }; // generate json containing category and clues. Loop here when the player guess
     console.log(roundDetails);
     return roundDetails;
 }
