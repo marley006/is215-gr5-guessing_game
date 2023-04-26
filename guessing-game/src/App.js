d1ea821b43a595727b97acbb17b3041cf94ec62c
Marla Vasquez <vasquez.marlasabado@gmail.com>
2023-04-26 14:46:17 +0800

    WIP: Move actual guessing logic (thanks Winnie!) to App.js
    ./Gameplay is not a react component which means cannot use state
    Move Winnie's code to app.js were the UI component resides and can use hooks.
    
    TODO: Setting of round details not immediately updated

--- guessing-game/src/App.js
+++ guessing-game/src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 import { newGame } from './services/Gameplay'
+import React, { useState } from 'react';
 
 function App() {
+  const [currentClueIndex, setCurrentClueIndex] = useState(0);    // count the number of clues provided
+  const [guess, setGuess] = useState('');                        // keep track of the user guess
+  const [isCorrect, setIsCorrect] = useState(false);             // tag correct guess
+  const [roundDetails, setRoundDetails] = useState([]);
+  const [hasDisplayedFirstClue, setHasDisplayedFirstClue] = useState(false);
+  const [hasStarted, setHasStarted] = useState(false);
 
-  const start = async () =>{
+  const start = async () => {
     console.log("creating new game");
-    newGame();
+    let payload = await newGame();
+    setRoundDetails(payload);
+    console.log(roundDetails);
+    console.log("game loaded");
+    setHasStarted(true);
+  }
+
+  function handleGuess(e) {
+    e.preventDefault();
+    if (guess.toLowerCase() === roundDetails.word.toLowerCase()) {
+      setIsCorrect(true);
+    } else {
+      setCurrentClueIndex(currentClueIndex + 1);
+      setGuess('');
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={start}>Start</button>
+        <div style={{ display: hasStarted ? "none" : "block" }}> 
+          <button onClick={start}>Start</button>
+        </div>
+        <div style={{ display: hasStarted ? "block" : "none" }}> 
+          <p>Category : {roundDetails.category}</p>
+        </div>
+        <div style={{ display: hasStarted ? "block" : "none" }}> 
+        { (hasStarted && roundDetails.length > 0) ?
+          <div>
+            <p>Clue #{currentClueIndex }: {roundDetails.clues[currentClueIndex]}</p>
+            <form onSubmit={handleGuess}>
+              <label>
+                <p/>
+                Guess:
+                <input type="text" value={guess} onChange={e => setGuess(e.target.value)} />
+              </label>
+              <button type="submit">Guess</button>
+            </form>
+          </div> :  ""
+        }
+        </div>
+
       </header>
     </div>
   );
--- guessing-game/src/services/Gameplay.js
+++ guessing-game/src/services/Gameplay.js
@@ -1,72 +1,19 @@
 import { generateWordByCategory, generateClues } from './OpenAiService'
 import { CATEGORIES } from './Constants';
-import React, { useState } from 'react';
 
-export const newGame = async () => {
+  export const newGame = async () => {
     console.log("preparing round");
-    const randomIndex =  Math.floor(Math.random() * 10); //randomize index here
+    const randomIndex = Math.floor(Math.random() * 10); //randomize index here
     const category = CATEGORIES[randomIndex]; // get category based on index
-    const  word =  await generateWordByCategory(category); // get clue from chatgpt
-    const  clues =  await generateClues(word, category); // get hints from chatgpt
+    const word = await generateWordByCategory(category); // get clue from chatgpt
+    const clues = await generateClues(word, category); // get hints from chatgpt
     const roundDetails = {
-        "categoy": category,
-        "word": word,
-        "clues": clues
-    }; // generate json containing category and clues. Loop here when the player guess
-
-    const [currentClueIndex, setCurrentClueIndex] = useState(0);    // count the number of clues provided
-    const [guess, setGuess] = useState('');                        // keep track of the user guess
-    const [isCorrect, setIsCorrect] = useState(false);             // tag correct guess
-    const [hasDisplayedFirstClue, setHasDisplayedFirstClue] = useState(false);
-
-    function handleGuess(e) {
-        e.preventDefault();
-    
-        // compare guess with the word
-        if (guess.toLowerCase() === word.toLowerCase()) {
-          setIsCorrect(true);
-        } else {                                                     // if incorrect, go to the next clue index
-          setCurrentClueIndex(currentClueIndex + 1);
-          setGuess('');
-        }
-      }
-
-      function handleDisplayFirstClue() {
-        setHasDisplayedFirstClue(true);
-      }
-  
-    return (
-     <div>
-        !hasDisplayedFirstClue ? (
-          <>
-            <p>Guess the word based on the following clue:</p>
-            <p>Clue #{currentClueIndex + 1}: {clues[currentClueIndex]}</p>
-            <button onClick={handleDisplayFirstClue}>Show First Clue</button>
-          </> ) 
-        :
-        {isCorrect ? (
-          <p>You guessed the word!</p>
-        ) : currentClueIndex === clues.length ? (
-          <p>You didn't guess the word. The word was "{word}".</p>
-        ) : (
-          <>
-            <p>Clue #{currentClueIndex + 1}: {clues[currentClueIndex]}</p>
-            <form onSubmit={handleGuess}>
-              <label>
-                Guess:
-                <input type="text" value={guess} onChange={e => setGuess(e.target.value)} />
-              </label>
-              <button type="submit">Guess</button>
-            </form>
-          </>
-        )} 
-    </div>
-    );
-
-    //console.log(roundDetails);
-    //return roundDetails;
-}
-
+      "categoy": category,
+      "word": word,
+      "clues": clues
+    };
+    return roundDetails;// generate json containing category and clues. Loop here when the player guess
+  }
 
 
 
